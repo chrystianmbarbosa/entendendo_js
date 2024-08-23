07ecbe09959705b2753f001cb58a3cde787bd203
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-08-22 21:52:38 -0300

    feat: Exercício 3 concluído

---
+++ exercicio3.js
@@ -0,0 +1,12 @@
+// Faça um algoritmo que peça dois números e imprima o maior deles.
+
+let numero1 = parseInt(prompt("Informe o Primeiro Numero:"));
+let numero2 = parseInt(prompt("Informe o Segundo Numero:"));
+
+if (numero1 > numero2) {
+    alert("O numero 1 é maior que numero 2.");
+} else {
+    alert("O numero 2 é maior que numero 1.");
+}
+
+
