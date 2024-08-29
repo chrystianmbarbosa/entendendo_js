26c541da3232173d03be2e3f7faa6aa6aa639de1
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-08-28 21:12:51 -0300

    fix: Corrigido Exercício 11

--- exercicio11.js
+++ exercicio11.js
@@ -12,12 +12,14 @@ let opcao = prompt(infoMenu);
 
 switch (opcao) {
     case "1":
-        celsius = parseInt("Informe quantos graus celsius a converter!");
+        celsius = parseFloat(
+            prompt("Informe quantos graus celsius a converter!"));
         fahrenheit = (celsius * 9 / 5) + 32;
         alert(`${celsius}ºC representa ${fahrenheit.toFixed(2)}ºF!`);
         break;
     case "2":
-        fahrenheit = parseInt("Informe quantos graus fahrenheit a converter!");
+        fahrenheit = parseFloat(
+            prompt("Informe quantos graus fahrenheit a converter!"));
         celsius = (fahrenheit - 32) * 5 / 9;
         alert(`${fahrenheit}ºF representa ${celsius.toFixed(2)}ºC!`);
         break;
